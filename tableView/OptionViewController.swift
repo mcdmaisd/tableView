246d89b82d8ae8f7fc96d084433d862d7b672f6c
mcdmaisd <tkim0156@gmail.com>
2024-07-22 09:27:17 +0900

    UICollectionViewListCell 적용하여 기존 코드 수정

--- tableView/OptionViewController.swift
+++ tableView/OptionViewController.swift
@@ -7,43 +7,93 @@
 
 import UIKit
 
+import SnapKit
+
+struct Options: Hashable, Identifiable {
+    let id = UUID()
+    let title: String
+}
+
+enum Section: String, CaseIterable {
+    case entire = "전체 설정"
+    case personal = "개인 설정"
+    case etc = "기타"
+}
+
 class OptionViewController: UIViewController {
     
-    @IBOutlet var tableview: UITableView!
+    private lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+    private var dataSource: UICollectionViewDiffableDataSource<Section, Options>!
+
+    let entire = [
+        Options(title: "공지사항"),
+        Options(title: "실험실"),
+        Options(title: "버전 정보")
+    ]
+    
+    let personal = [
+        Options(title: "개인/보안"),
+        Options(title: "알림"),
+        Options(title: "채팅"),
+        Options(title: "멀티프로필")
+    ]
+    
+    let etc = [
+        Options(title: "고객센터/도움말")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureTableView()
-        registerXib()
+        navigationItem.title = "설정"
+        view.addSubview(collectionView)
+        collectionView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        configureDataSource()
+        updateSnapshot()
     }
     
-    func configureTableView() {
-        tableview.dataSource = self
-        tableview.delegate = self
-        tableview.rowHeight = 50
+    
+    func configureDataSource() {
+        var registration: UICollectionView.CellRegistration<UICollectionViewCell, Options>!
+        registration = UICollectionView.CellRegistration { cell, indexPath, itemIdentifier in
+            var content = UIListContentConfiguration.valueCell()
+            content.text = itemIdentifier.title
+                        
+            var backgroundConfig = UIBackgroundConfiguration.listGroupedCell()
+            backgroundConfig.backgroundColor = .white
+            
+            cell.contentConfiguration = content
+            cell.backgroundConfiguration = backgroundConfig
+        }
+        
+        dataSource = UICollectionViewDiffableDataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
+            
+            let cell = collectionView.dequeueConfiguredReusableCell(using: registration, for: indexPath, item: itemIdentifier)
+            return cell
+        })
     }
     
-    func registerXib()  {
-        let xib = UINib(nibName: SettingTableViewCell.identifier, bundle: nil)
-        tableview.register(xib, forCellReuseIdentifier: SettingTableViewCell.identifier)
+    func updateSnapshot() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Options>()
+        snapshot.appendSections(Section.allCases)
+        snapshot.appendItems(entire, toSection: .entire)
+        snapshot.appendItems(personal, toSection: .personal)
+        snapshot.appendItems(etc, toSection: .etc)
+        dataSource.apply(snapshot)
+    }
+
+    
+    func createLayout() -> UICollectionViewLayout {
+        var configuration = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+        
+        configuration.backgroundColor = .clear
+        configuration.showsSeparators = false
+        
+        let layout = UICollectionViewCompositionalLayout.list(using: configuration)
+        
+        return layout
     }
 }
 
-extension OptionViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        Options.allCases[section].cellTitle.count
-    }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        Options.allCases[section].sectionTitle
-    }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        Options.allCases.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: SettingTableViewCell.identifier, for: indexPath) as! SettingTableViewCell
-        cell.setLabel(indexPath)
-        return cell
-    }
-}
